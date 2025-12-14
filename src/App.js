807f4708ac76f4d6bd353eba579c2a5b5b743aa4
parastoo <parastookachkoly@gmail.com>
2025-12-14 10:24:09 +0330

    new test

--- src/App.js
+++ src/App.js
@@ -3,23 +3,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyComponent 
+        name="Sample Name" 
+        description="This is a sample description." 
+        text="Additional text goes here." 
+      />
+      <MyComponent 
+        name="Another Name" 
+        description="This is another description." 
+        text="More additional text." 
+      />
+
+      <MyComponent 
+        name="Third Name" 
+        description="This is the third description." 
+        text="Even more additional text." 
+      />
+
     </div>
-  );
+  )
+}
+
+const MyComponent = (props) => {
+  return(
+    <div>
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+      <span>{props.text}</span>
+    </div>
+  )
 }
 
 export default App;
