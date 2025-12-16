96964ae96b3e2a6f60f24cad020500c55185e6ca
parastoo <parastookachkoly@gmail.com>
2025-12-16 10:57:01 +0330

    add if

--- src/App.js
+++ src/App.js
@@ -2,39 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const age = 20;
+
   return (
-    <div>
-      <MyComponent 
-        name="Sample Name" 
-        description="This is a sample description." 
-        text="Additional text goes here." 
-      />
-      
-      <MyComponent 
-        name="Another Name" 
-        description="This is another description." 
-        text="More additional text." 
-      />
-
-      <MyComponent 
-        name="Third Name" 
-        description="This is the third description." 
-        text="Even more additional text." 
-      />
-
+    <div className="App">
+      {age >= 18 ? <h1> over </h1> : <h1> under </h1>}
+      {age === 20 && <h1>20years old</h1>}
+      <button type="submit" className={age >= 20 ? "success-message" : "unsuccess-message"}>Click Me!</button>
     </div>
   )
+
+
+  
 }
-
-
-const MyComponent = (props) => {
-  return(
-    <div className="my-component">
-      <h1>{props.name}</h1>
-      <p>{props.description}</p>
-      <span>{props.text}</span>
-    </div>
-  )
-}
-
 export default App;
